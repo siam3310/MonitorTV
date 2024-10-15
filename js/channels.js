d95578e65f80317ea468a57ea1a1bee5eca86ad8
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-10-15 13:24:47 +0600

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -1,16 +1,16 @@
 let LosCanales = [
     {
-        'titulo': '24 PLAY',
-        'id': '24PLAY',
+        'titulo': 'BPL 2024 Draft',
+        'id': 'bpldraft',
         'nombre': '<span style="margin-right: 10px;" class="material-icons-round TextoIconoAzul">tv</span><img id="Logo" style="height: 15px; width: auto; margin-right: 10px;" src="Imagenes/Logo_CANAL24H.svg"></img>',
-        'tags' : ['Chile','Noticias'],
-        'url': '../Monitores/Senal/24HTVN_IFRAME.html'
+        'tags' : ['Sports','Bangla'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
     },
     {
         'titulo': 'SPACEX MISSIONS',
         'id': 'SPACEXMISSIONS_YT',
         'nombre': '<img id="Logo" style="height: 15px; width: auto; margin-right: 10px;" src="Imagenes/Logo_YT.svg"></img><span style="margin-right: 10px;" class="material-icons-round">photo_camera</span>SPACEX MISSIONS ',
         'tags' : ['Espacio'],
-        'url': 'https://www.youtube.com/embed/live_stream?channel=UCE_z1X3v8ZdBNHn9IFHBfNw&autoplay=true&mute=1'
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=UCE_z1X3v8ZdBNHn9IFHBfNw'
     },
 ]
