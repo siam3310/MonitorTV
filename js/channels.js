a9a8c866e4d2fb0a865fd20accfb66b149c1eed9
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-10-15 15:22:03 +0600

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -1,10 +1,10 @@
 let LosCanales = [
     {
-        'titulo': 'BPL Draft 2024',
-        'id': 'bpldraft',
-         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BPL Draft',
+        'titulo': 'TSPORTS ',
+        'id': 'tsports',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>TSPORTS',
         'tags' : ['Sports','Bangla'],
-        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https%3A%2F%2Ffifabd.xyz%2FTTT%2Fplay.m3u8%3Fid%3D2%26q%3D720'
     },
     {
         'titulo': 'BPL Draft 2024',
@@ -13,40 +13,5 @@ let LosCanales = [
         'tags' : ['Sports','Bangla'],
         'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
     },
-        {
-        'titulo': 'BPL Draft 2024',
-        'id': 'bpldraft',
-         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BPL Draft',
-        'tags' : ['Sports','Bangla'],
-        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
-    },
-        {
-        'titulo': 'BPL Draft 2024',
-        'id': 'bpldraft',
-         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BPL Draft',
-        'tags' : ['Sports','Bangla'],
-        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
-    },
-        {
-        'titulo': 'BPL Draft 2024',
-        'id': 'bpldraft',
-         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BPL Draft',
-        'tags' : ['Sports','Bangla'],
-        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
-    },
-                {
-        'titulo': 'BPL Draft 2024',
-        'id': 'bpldraft',
-         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BPL Draft',
-        'tags' : ['Sports','Bangla'],
-        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
-    },
-
-        {
-        'titulo': 'BPL Draft 2024',
-        'id': 'bpldraft',
-         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BPL Draft',
-        'tags' : ['Sports','Bangla'],
-        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
-    },
+     
 ]
