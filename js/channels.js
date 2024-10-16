832b579b61fedf3420d591778189fc12e616a3a2
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-10-16 13:36:43 +0600

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -18,11 +18,107 @@ let LosCanales = [
         'tags' : ['Sports','Bangla'],
         'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https%3A%2F%2Ffifabd.xyz%2FTTT%2Fplay.m3u8%3Fid%3D2%26q%3D720'
     },  {
-        'titulo': 'TSPORTS  ',
+        'titulo': 'Siam  ',
         'id': 'tsports2',
          'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>TSPORTS 2',
         'tags' : ['Sports','Bangla'],
         'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://freecatv.vercel.app/tsports.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'sportstv1',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>SPORT TV 1',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://8r1fp7ro6am.a.trbcdn.net/SportTV1/tracks-v4a1/mono.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports1',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 1',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports1.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports1',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 2',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports2.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports3',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 3',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports3.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports4',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 4',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports4.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports5',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 5',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports5.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports6',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 6',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports6.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports7',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 7',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports7.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports8',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 8',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports8.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'beinsports9',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BEIN SPORTS 9',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://live2.beinconnect.us/YallaGoalApp/beINSports9.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'sportstv2',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>SPORTS TV 2',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://8r1fp7ro6am.a.trbcdn.net/SportTV2/index.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'sportstv3',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>SPORTS TV 3',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://8r1fp7ro6am.a.trbcdn.net/SportTV3/index.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'sportstv4',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>SPORTS TV 4',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://8r1fp7ro6am.a.trbcdn.net/SportTV4/index.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'sportstv5',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>SPORTS TV 5',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://8r1fp7ro6am.a.trbcdn.net/SportTV5/index.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'sportstv6',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>SPORTS TV 6',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://8r1fp7ro6am.a.trbcdn.net/SportTV6/index.m3u8'
+    },  {
+        'titulo': 'Siam  ',
+        'id': 'sportstv2',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>SPORTS TV 2',
+        'tags' : ['Sports'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://8r1fp7ro6am.a.trbcdn.net/SportTV2/index.m3u8'
     },
 
 
