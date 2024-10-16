bde3176b1dc3a05a531c9cace729192e6dba7b12
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-10-16 13:27:11 +0600

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -1,21 +1,24 @@
 let LosCanales = [
-    {
+        {
+        'titulo': 'BPL Draft 2024',
+        'id': 'bpldraft',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>BPL Draft',
+        'tags' : ['Sports','Bangla'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=yt&link=mRYahD53jfI'
+    },  {
         'titulo': 'India Today',
         'id': 'indiatoday',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons-round TextoIconoAzul">tv</span>',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons-round TextoIconoAzul">tv</span> India Today',
         'tags' : ['News','Indian'],
         'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://indiatodaylive.akamaized.net/hls/live/2014320/indiatoday/indiatodaylive/playlist.m3u8'
-    }, {
-        'titulo': 'CNN CHILE',
-        'id': 'CNN_CHILE',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span><img id="Logo" style="height: 20px; width: auto; margin-right: 10px;" src="Imagenes/Logo_CNNCHILE2.svg"></img>',
-        'tags' : ['Chile','Noticias'],
-        'url': '../Monitores/Senal/CNNCHILE.html'
-    },{
-        'titulo': 'CHV NOTICIAS',
-        'id': 'CHVNOTICIAS',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span><img id="Logo" style="height: 20px; width: auto; margin-right: 10px;" src="Imagenes/Logo_CHV_NOTICIAS.svg"></img><span title="AGREGADO RECIENTEMENTE" style="margin-left: 0px;" class="BotonAvisoListaSeñales TextoIconoVerdeClaro"><span style="font-size: 17px" class="material-icons-round">auto_awesome</span><span style="font-size: 12px; margin-left: 5px;">NUEVO</span></span>',
-        'tags' : ['Chile','Noticias'],
-        'url': '../Monitores/Senal/CHVNOTICIAS.html'
     },
+
+
+
+
+
+
+
+
+    
 ]
