d32c037f599522815745f0fea76f4a66b9483a16
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-10-16 13:28:44 +0600

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -11,6 +11,18 @@ let LosCanales = [
         'nombre': '<span style="margin-right: 10px;" class="material-icons-round TextoIconoAzul">tv</span> India Today',
         'tags' : ['News','Indian'],
         'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://indiatodaylive.akamaized.net/hls/live/2014320/indiatoday/indiatodaylive/playlist.m3u8'
+    },  {
+        'titulo': 'TSPORTS ',
+        'id': 'tsports1',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>TSPORTS 1',
+        'tags' : ['Sports','Bangla'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https%3A%2F%2Ffifabd.xyz%2FTTT%2Fplay.m3u8%3Fid%3D2%26q%3D720'
+    },  {
+        'titulo': 'TSPORTS  ',
+        'id': 'tsports2',
+         'nombre': '<span style="margin-right: 10px;" class="material-icons-round">tv</span>TSPORTS 2',
+        'tags' : ['Sports','Bangla'],
+        'url': 'https://freecatv.pages.dev/gdplayer?player=plyr&provider=rand&format=video%2Fmp4&link=https://freecatv.vercel.app/tsports.m3u8'
     },
 
 
